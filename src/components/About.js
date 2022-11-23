64ae18f1b1abd398b319c3cc17594375a23c2137
jchoi10 <noreply>
2022-11-23 00:35:24 -0500

    add about section

---
+++ src/components/About.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const About = () => {
+    return(
+        <section id="about" class="intro">
+            <div class="flex-row">
+                <h2 class="section-title primary-border">About</h2>
+            </div>  
+            <div class="flex-row">
+                <p>
+                <b>Jongwon (Michael) Choi</b> (Republic of Korea, 1989) is a designer, developer and educator. He currently works at Rice+Lipka Architects as an architectural designer. Jongwon graduated in 2019 with M.Arch degree from Columbia University Graduate School of Architecture, Planning and Preservation.<br/>
+                Jongwon likes bouldering/indoor climbing, swimming, travel and film photography.
+                </p>
+            </div>
+        </section>
+    )
+};
+
+export default About;
