704742cd1e804683cff439014da33346af26b6b6
jchoi10 <noreply>
2022-11-25 02:46:31 -0500

    about page style

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -3,12 +3,12 @@ import coverImage from "../../assets/cover/hero_background.JPG";
 
 const About = () => {
     return(
-        <section id="about" class="intro">
-            <div class="flex-row">
+        <section id="about" class="intro text-center">
+            <div class="flex-row mb-3">
                 <h2 class="section-title primary-border">About</h2>
             </div>  
             <div class="flex-row">
-                <img src={coverImage} className="mx-2" style={{width: "50%"}} alt="cover" />
+                <img src={coverImage} className="mx-2 mb-3" style={{width: "25%"}} alt="cover" />
                 <p>
                 <b>Jongwon (Michael) Choi</b> (Republic of Korea, 1989) is a designer, developer and educator. He currently works at Rice+Lipka Architects as an architectural designer. Jongwon graduated in 2019 with M.Arch degree from Columbia University Graduate School of Architecture, Planning and Preservation.<br/>
                 Jongwon likes bouldering/indoor climbing, swimming, travel and film photography.
