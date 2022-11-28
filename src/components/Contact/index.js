891082185314058b154d7101e350622aaa82cecb
jchoi10 <noreply>
2022-11-28 01:00:53 -0500

    updated contact code

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -2,34 +2,37 @@ import React, {useState} from "react";
 
 const ContactForm = () => {
     const [formState, setFormState] = useState({
-        name:"",
-        email:"",
-        message:""
+        name:'',
+        email:'',
+        message:''
     });
     
     const {name, email, message} = formState;
-
+    
     function handleChange(e) {
-        setFormState({...formState, [e.target.name]: e.target.value})
+        setFormState({...formState, [e.target.name]: e.target.value })
     };
 
-    console.log(formState);
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log(formState);
+    };
 
     return (
         <section id="contact">
             <h1>Contact me</h1>
-            <form id="contact-form">
+            <form id="contact-form" onSubmit={handleSubmit}>
                 <div>
                     <label htmlFor="name">Name:</label>
                     <input type="text" defaultValue={name} onChange={handleChange} name="name" />
                 </div>
                 <div>
                     <label htmlFor="email">Email address:</label>
-                    <input type="email" defaultValue={email} onChange={handleChange} name="email" />
+                    <input type="email" defaultValue={email} name="email" onChange={handleChange} />
                 </div>
                 <div>
                     <label htmlFor="message">Message:</label>
-                    <textarea type="message" defaultValue={message} onChange={handleChange} rows="5" />
+                    <textarea name="message" defaultValue={message} onChange={handleChange} rows="5" />
                 </div>
                 <button type="submit">Submit</button>
             </form>
